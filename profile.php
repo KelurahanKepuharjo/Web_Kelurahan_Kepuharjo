2b5ca47660aec1862dc29cfc3fb2453004386f88
Nadia Ayu <nadiaayus31@gmail.com>
2022-11-28 22:29:05 +0700

    nadia profile dan kelurahan

--- profile.php
+++ profile.php
@@ -9,12 +9,26 @@ include('include/navbar.php');
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Profil</h1>
 
-    <div class="row">
-
+    
+    <form method="post">
         <div class="col-lg-6">
+        <div class=form-group>
+            <label>Nama</label>
+            <input type="text" name="username" class="form-control">
         </div>
-
-    </div>
+        <div class=form-group>
+            <label>Email</label>
+            <input type="email" name="email" class="form-control" >
+        </div>
+        <div class=form-group>
+            <label>Status</label>
+            <input type="text" name="status" class="form-control">
+        </div>
+        </div>
+        <button type="submit" name="update" class="btn btn-primary"> Update Profile</button>
+        </div>
+    </form>
+    
     <!-- /.container-fluid -->
 
 </div>
