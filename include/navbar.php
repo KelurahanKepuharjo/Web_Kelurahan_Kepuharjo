45277db3463c6425a0fbad76f252eaa34e540d26
FaisalOktabrianSholihin <faisalokta08@gmail.com>
2022-12-01 09:52:34 +0700

    update logout

--- include/navbar.php
+++ include/navbar.php
@@ -237,8 +237,8 @@
                       Kelurahan
                     </a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="login.php">
-                      <i class="fa fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                      <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                       Logout
                     </a>
                   </div>
@@ -246,6 +246,23 @@
               </ul>
             </nav>
             <!-- End of Topbar -->
+            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Apakah Anda Ingin keluar?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">Maka anda akan diarahkan ke halaman landing page.</div>
+                  <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Tidak</button>
+                    <a class="btn btn-primary" href="landingpage.php">Iya</a>
+                  </div>
+                </div>
+              </div>
+            </div>
 
             <!-- Begin Page Content -->
             <div class="container-fluid">
