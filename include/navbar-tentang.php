77779e3d58e7e8e1709eb1c359f536e0acba4be7
EdyAtthoillah123 <112244332+EdyAtthoillah123@users.noreply.github.com>
2022-12-10 20:29:02 +0700

    modal

--- include/navbar-tentang.php
+++ include/navbar-tentang.php
@@ -216,6 +216,23 @@
               </ul>
 
             </nav>
+            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Apakah Anda Ingin keluar?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">Maka anda akan diarahkan ke halaman landing page.</div>
+                  <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Tidak</button>
+                    <a class="btn btn-primary" href="../Web_Kelurahan_Kepuharjo/Logout.php">Iya</a>
+                  </div>
+                </div>
+              </div>
+            </div>
             <!-- End of Topbar -->
 
             <!-- Begin Page Content -->
